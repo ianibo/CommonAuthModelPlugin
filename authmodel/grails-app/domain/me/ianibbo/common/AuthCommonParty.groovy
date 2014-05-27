ba714cb2992fe7ac1ff8fd2c1e82d0fc08847f84
Ian Ibbotson <ian.ibbotson@k-int.com>
2014-05-27 09:24:25 +0100

    Corrected Mapping on Party domain object

--- authmodel/grails-app/domain/me/ianibbo/common/AuthCommonParty.groovy
+++ authmodel/grails-app/domain/me/ianibbo/common/AuthCommonParty.groovy
@@ -11,14 +11,14 @@ class AuthCommonParty {
   static mapping = {
     table name:'auth_common_party', schema:'common'
     notes type:'text'
-    email(nullable:true, blank:true)
-    twitter(nullable:true, blank:true)
-    facebook(nullable:true, blank:true)
   }
 
   static constraints = {
     displayName nullable:true, blank: true
     notes nullable:true, blank: true
+    email nullable:true, blank:true
+    twitter nullable:true, blank:true
+    facebook nullable:true, blank:true
   }
 
 }
